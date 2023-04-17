c9f6e3f8cf5d70485d9a47559dec9b496e7b0d36
hsnv55 <ehtiram.hesenov135@gmail.com>
2023-04-17 17:05:38 +0500

    ghjk

--- Ass.tsx
+++ Ass.tsx
@@ -6,15 +6,18 @@
  */
 
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
 const Ass = () => {
     return (
         <View style={
             styles.backround
         } >
-            <Image style={{marginLeft:20,width:22,height:22,marginTop:23, marginVertical:10,}} source={require('./image/Menu.png')}/>
-            <Image style={{marginLeft:300,width:22,height:22, marginTop:-36}} source={require('./image/search.png')}/>
-            <Image style={{marginLeft:360,width:22,height:22, marginTop:-25,}} source={require('./image/magaza.png')}/>
+            <TouchableOpacity> 
+                <Image style={{marginLeft:20,width:22,height:22,marginTop:23, marginVertical:10,}} source={require('./image/Menu.png')}/></TouchableOpacity>
+            <TouchableOpacity> 
+                 <Image style={{marginLeft:300,width:22,height:22, marginTop:-36}} source={require('./image/search.png')}/></TouchableOpacity>
+            <TouchableOpacity> 
+                 <Image style={{marginLeft:360,width:22,height:22, marginTop:-25,}} source={require('./image/magaza.png')}/></TouchableOpacity>
             <Text style={
                 styles.textstile
 
@@ -26,42 +29,47 @@ const Ass = () => {
         fontWeight: '300',
         marginLeft:16,
     fontFamily:'SofiaPro-Bold',}}>AT YOUR DOOR STEPS</Text>
+<TouchableOpacity>
      <Image style={{  height: 66,
 width: 66,
 marginLeft: 10,
 marginTop: 5,
-
-}} source={require('./image/17.png')}/>
-    
+}} source={require('./image/17.png')}/></TouchableOpacity>
+    <TouchableOpacity>
     <Image style={{  height: 66,
 width: 66,
 marginLeft: 110,
 marginTop: -65,
 
-}} source={require('./image/bowl.png')}/>
+}} source={require('./image/bowl.png')}/></TouchableOpacity>
+   <TouchableOpacity> 
     <Image style={{ height: 66,
 width: 66,
 marginTop :-70,
 marginLeft:220
 
-}} source={require('./image/doner.png')}/>
-    <Image style={{height: 66,
+}} source={require('./image/doner.png')}/></TouchableOpacity>
+  <TouchableOpacity>
+     <Image
+     style={{height: 66,
 width: 66,
 marginLeft: 320,
 marginTop: -70,
 
-}} source={require('./image/marojna.png')}/>
+}} source={require('./image/marojna.png')}/></TouchableOpacity>
     
             <Image  style={{ position: 'absolute',
-            rowGap:23,
+            
 width: 200,
 height: 250,
 marginLeft:50,
 left: -40,
 top: 335,}} source={require('./image/2.png')} />
-          <Image style={styles.imagestyle} source={require('./image/3.png')}/>
-          <Image style={{marginTop:530,marginLeft:25,position: 'absolute'}} source={require('./image/Orice.png')}/> 
-          <Image style={{marginTop:530,marginLeft:160,position: 'absolute'}} source={require('./image/buton.png')}/> 
+             <Image style={styles.imagestyle} source={require('./image/3.png')}/>
+       
+             <Image style={{marginTop:530,marginLeft:25,position: 'absolute'}} source={require('./image/Orice.png')}/> 
+         
+             <Image style={{marginTop:530,marginLeft:160,position: 'absolute'}} source={require('./image/buton.png')}/> 
           <Image style={{marginTop:530,marginLeft:260,position: 'absolute'}} source={require('./image/Oric1.png')}/> 
           
 <Image style={{marginTop:400,marginLeft:25}} source={require('./image/now.png')}/>
@@ -81,7 +89,8 @@ color:'#333333',
 marginLeft: 150,
 marginTop: 725,
 }}>$110.50</Text>
-<Image style={{marginTop:7,marginLeft:340}} source={require('./image/isaresiz.png')}/>
+<TouchableOpacity>
+<Image style={{marginTop:7,marginLeft:340}} source={require('./image/isaresiz.png')}/></TouchableOpacity>
         </View>
     )
 }
